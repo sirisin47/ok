cabd1e682f5ad20c23cbae01c730817f275c6273
sirisin47 <gusnewtan09@gmail.com>
2024-01-24 10:30:41 +0700

    .

--- Streamlit01.py
+++ Streamlit01.py
@@ -6,7 +6,11 @@ st.header("การนำเสนอข้อมูลกราฟด้วย
 col1, col2, col3 = st.columns(3)
 
 with col1:
-
+    st.header("Versicolor")
+    st.image("./img2/Versicolor.jpg")
 with col2:
-
+    st.header("Verginica")
+    st.image("./img2/Verginica.jpg")
 with col3:
+    st.header("Setosa")
+    st.image("./img2/Sentosa.jpg")
