b3957b3fe48f2aacea39a156bd5a03c056ff25d5
sirisin47 <gusnewtan09@gmail.com>
2024-01-24 11:09:05 +0700

    Update Streamlit01.py

--- Streamlit01.py
+++ Streamlit01.py
@@ -18,7 +18,7 @@ df=pd.read_csv("./data/iris.csv")
 
 if(st.button("แสดงข้อมูลตัวอย่าง")):
     st.write(df.head(10))
-    st.button(."ไม่แสดงข้อมูลตัวอย่าง")
+    st.button("ไม่แสดงข้อมูลตัวอย่าง")
 else:
     st.button("ไม่แสดงข้อมูลตัวอย่าง")
 
