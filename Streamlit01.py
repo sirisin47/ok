a59f9f3abe781042444dcef1fd3b70b8f0e452c0
sirisin47 <gusnewtan09@gmail.com>
2024-01-24 11:14:50 +0700

    Update Streamlit01.py

--- Streamlit01.py
+++ Streamlit01.py
@@ -22,4 +22,15 @@ if(st.button("แสดงข้อมูลตัวอย่าง")):
 else:
     st.button("ไม่แสดงข้อมูลตัวอย่าง")
 
-    
+    if(st.button("แสดงกราฟแท่ง")):
+        chart_data = pd.DataFrame(
+    {
+        "ประเภทดอกไม้": df['variety'],
+        "ความกว้าง": df['sepal.width'],
+        "ความยาว": df['sepal.length']    
+        }
+    )
+    st.bar_chart(chart_data, x="ประเภทดอกไม้", y=["ความกว้าง","ความยาว"], color=["#FF0000", "#0000FF"])
+    st.button("ไม่แสดงข้อมูลสถิติ")
+else:
+    st.button("ไม่แสดงข้อมูลสถิติ")
